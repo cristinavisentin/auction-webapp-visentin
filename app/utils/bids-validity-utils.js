c457844dbac7da6bcdb42256dcadb6765b75d6c3
cristinavisentin <c.visentin00@gmail.com>
2025-01-06 11:44:00 +0100

    remove comment

--- app/utils/bids-validity-utils.js
+++ app/utils/bids-validity-utils.js
@@ -4,7 +4,7 @@ const isValid = (lastValue, newValue) => {
 
 const getHighestBidValue = (bids, initialValue) => {
     if (!bids || bids.length === 0) {
-        return parseFloat(initialValue); // Se non ci sono bid, usa il valore iniziale
+        return parseFloat(initialValue);
     }
     return bids.reduce((max, bid) => Math.max(max, parseFloat(bid.value)), parseFloat(initialValue));
 };
